28d262f709c304beaf9645a84ce0ee112009419a
Michael Jonker <michael@openpoint.ie>
2019-06-15 21:10:36 +0100

    add activities service

--- src/pubsubProxy.js
+++ src/pubsubProxy.js
@@ -1,18 +1,15 @@
 const config = require('config');
-
-// eslint-disable-next-line no-underscore-dangle
-global.__network = config.get('network');
-
 const zmq = require('zmq');
 
+const network = config.get('network');
 const publisher = zmq.socket('pub');
-console.log('zmq publisher: ', `tcp://${__network.pubsub.host}:${__network.pubsub.port}`);
-publisher.bindSync(`tcp://${__network.pubsub.host}:${__network.pubsub.port}`);
+console.log('zmq publisher: ', `tcp://${network.pubsub.host}:${network.pubsub.port}`);
+publisher.bindSync(`tcp://${network.pubsub.host}:${network.pubsub.port}`);
 
 function makeProxy(service) {
     const listener = zmq.socket('pull');
-    console.log(`${service} listener: `, `tcp://${__network[service].host}:${__network[service].publish}`);
-    listener.connect(`tcp://${__network[service].host}:${__network[service].publish}`);
+    console.log(`${service} listener: `, `tcp://${network[service].host}:${network[service].publish}`);
+    listener.connect(`tcp://${network[service].host}:${network[service].publish}`);
     listener.on('message', (msg) => {
         try {
             const message = JSON.parse(msg.toString());
